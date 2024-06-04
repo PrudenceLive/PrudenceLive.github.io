2efaecf7a75b88d97d02e3de5e9c875648ba7a9f
prudencelive <129216286+PrudenceLive@users.noreply.github.com>
2024-06-04 15:10:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // JavaScript code for handling navigation, carousel initialization, etc.
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("defaultOpen").click();
+
+    // Toggle password visibility
+    const togglePassword = document.querySelectorAll('.toggle-password');
+    togglePassword.forEach(item => {
+        item.addEventListener('click', function () {
+            const input = this.previousElementSibling;
+            const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
+            input.setAttribute('type', type);
+            this.classList.toggle('fa-eye-slash');
+        });
+    });
+
+    // Open the tab on click
+    document.querySelectorAll('.tablinks').forEach(button => {
+        button.addEventListener('click', function(event) {
+            openTab(event, this.textContent);
+        });
+    });
+
+    function openTab(event, tabName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(tabName).style.display = "block";
+        event.currentTarget.className += " active";
+    }
+
+    // Smooth scroll for navigation links
     const links = document.querySelectorAll('.menu-hover');
     links.forEach(link => {
         link.addEventListener('click', function (e) {
@@ -15,6 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
+    // Initialize carousel
     $(".owl-carousel").owlCarousel({
         items: 3,
         margin: 10,
